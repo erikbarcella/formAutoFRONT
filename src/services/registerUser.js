5f34775accc0bb37f76d2e30132232eb3ad14ffe
Erik <57962093+erikbarcella@users.noreply.github.com>
2024-04-21 18:11:34 -0300

    up
    up

--- src/services/registerUser.js
+++ src/services/registerUser.js
@@ -2,14 +2,14 @@ import api from './api';
 
 const registerUser = async (userData) => {
     try {
-        console.log("chamando api ")
-        console.log(userData);
         const response = await api.post('/user/register',
             JSON.stringify({userData}),
             { headers: { 'Content-Type': 'application/json' } }
         );
+        console.log("API RESPONSE =>>>", response.data)
         return response.data;
     } catch (error) {
+        console.log("API ERROR =>>>", error)
         let err;
         if(error.response.data) err = error.response;
         else err = error;
