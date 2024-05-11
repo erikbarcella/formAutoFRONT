e68a5703e29148486f1990af8dc2105efe99efe3
Erik <57962093+erikbarcella@users.noreply.github.com>
2024-05-11 14:49:21 -0300

    implements role auth
    implements role auth

--- src/components/AppContent.js
+++ src/components/AppContent.js
@@ -1,19 +1,30 @@
 import React, { Suspense } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { CContainer, CSpinner } from '@coreui/react'
-
+import { useAuth } from '../context/AuthContext';
 // routes config
 import routes from '../routes'
 
 // Implementar estrategia de AUTH
 const AppContent = () => {
+  const { user } = useAuth();
+
+  const isAuthorized = (route) => {
+    if (!route.allowedRoles || route.allowedRoles.length === 0) {
+      // Se não há roles definidas, a rota é acessível por padrão
+      return true;
+    }
+    // Verifique se o usuário tem pelo menos uma das roles permitidas
+    return user && user.roles && route.allowedRoles.some(role => user.roles.includes(role));
+  };
+
   return (
     <CContainer className="px-4" lg>
       <Suspense fallback={<CSpinner color="primary" />}>
         <Routes>
-          {routes.map((route, idx) => {
-            return (
-              route.element && (
+        {routes.map((route, idx) => {
+            if (route.element && isAuthorized(route)) {
+              return (
                 <Route
                   key={idx}
                   path={route.path}
@@ -21,11 +32,11 @@ const AppContent = () => {
                   name={route.name}
                   element={<route.element />}
                 />
-              )
-            )
+              );
+            }
+            return null;
           })}
-          {/* <Route path="/" element={<Navigate to="login" replace />} /> */}
-          { <Route path="/" element={<Navigate to="dashboard" replace />} />}
+          <Route path="/" element={<Navigate to="dashboard" replace />} />
         </Routes>
       </Suspense>
     </CContainer>
